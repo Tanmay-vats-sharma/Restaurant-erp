089e83e9d3a9795b8da54296ba9f48d6f1139951
Tanmay <tanmaysharmats36@gmail.com>
2025-11-05 12:29:38 +0530

    Initial Setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+
+  return (
+    <>
+      <div className="text-3xl font-bold underline text-red-500">
+        <p>Tanmay sharma</p>
+
+      </div>
+    </>
+  )
+}
+
+export default App
