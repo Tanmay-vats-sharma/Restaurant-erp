02aa0ed4a6b3b0fcfd35160586930304d81acd6b
Tanmay <tanmaysharmats36@gmail.com>
2025-11-08 22:05:04 +0530

    Network Hosting enabled

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,4 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // listen on all interfaces (equivalent to 0.0.0.0)
+    port: 5173  // optional: set explicit port
+  }
 })
